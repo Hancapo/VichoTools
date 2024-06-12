ec1fe93c57b3a05813836b2344d4d419b332adcf
Vicente <vicho0202@gmail.com>
2024-06-12 16:48:19 -0400

    fix: better dotnet detection

--- vicho_dependencies.py
+++ vicho_dependencies.py
@@ -1,19 +1,18 @@
 from pathlib import Path
-import subprocess
 import sys
+import os
 
 
 def is_dotnet_installed():
-    try:
-        result = subprocess.run(['dotnet', '--version'], capture_output=True, text=True, check=True)
-        print(f'Installed .NET version: {result.stdout}')
-        return True
-    except subprocess.CalledProcessError:
-        print('Error: .NET is not installed')
-        return False
-    except FileNotFoundError:
-        print('Error: .NET is not installed')
-        return False
+    path_env = os.getenv('PATH')
+    paths = path_env.split(os.pathsep)
+    for path in paths:
+        dotnet_path = os.path.join(path, 'dotnet')
+        if os.path.isfile(dotnet_path) or os.path.isfile(dotnet_path + '.exe'):
+            print('.NET is installed.')
+            return True
+    print('Error: .NET is not installed')
+    return False
 
 
 def depen_installed():
