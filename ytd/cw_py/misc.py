a7f0de14c913d7e36a0ade2f26d39d36bf26b74b
Vicente <vicho0202@gmail.com>
2024-08-13 04:35:16 -0400

    feat: dds quality improve

--- ytd/cw_py/misc.py
+++ ytd/cw_py/misc.py
@@ -1,20 +1,21 @@
 from pathlib import Path
 
+SUPPORTED_FORMATS = [".png", ".jpg", ".bmp", ".tiff", ".tif", ".jpeg", ".dds", ".psd", ".gif", ".webp"]
+
 
 def get_folder_list_from_dir(dir: str):
     return [str(p) for p in Path(dir).rglob("*") if p.is_dir()]
 
 
 def get_non_dds(path: str) -> list[str]:
-    supported_formats = [".png", ".jpg", ".jpeg", ".tga", ".bmp", "tif", "tiff"]
-    return [str(p) for p in Path(path).rglob("*") if p.suffix in supported_formats]
+    return [str(p) for p in Path(path).rglob("*") if p.suffix in SUPPORTED_FORMATS]
 
 
 def get_dds(path: str) -> list[str]:
     return [str(p) for p in Path(path).rglob("*.dds")]
 
 
-def calculate_mipmaps(width: int, height: int) -> int:
+def calculate_mipmaps_lvls(width: int, height: int) -> int:
     if width <= 4 or height <= 4:
         return 1
     levels = 1
