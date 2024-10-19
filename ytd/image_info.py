d1db4eae08839635053ac159a2a021a472d998cb
MrVicho13 <vicho0202@gmail.com>
2024-10-19 01:30:45 -0300

    tweak: image_info docstring changes

--- ytd/image_info.py
+++ ytd/image_info.py
@@ -2,6 +2,7 @@ from typing import NamedTuple
 from pathlib import Path
 
 class ImageInfo(NamedTuple):
+    """A class to store information about an image"""
     img_path: str
     material: str
     flag_tint: bool = False
@@ -10,15 +11,15 @@ class ImageInfo(NamedTuple):
  
     @property
     def img_name_full(self) -> str:
-        """Returns the full name of the image including the extension"""
+        """Returns image's full name including its extension"""
         return Path(self.img_path).name if self.img_path else ''
 
     @property
     def img_ext(self) -> str:
-        """Returns the extension of the image"""
+        """Returns image's extension"""
         return Path(self.img_path).suffix if self.img_path else ''
     
     @property
     def img_name(self) -> str:
-        """Returns the name of the image without the extension"""
+        """Returns image's name without its extension"""
         return Path(self.img_path).stem if self.img_path else ''
