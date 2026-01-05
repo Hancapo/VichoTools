4f2ee06af04049c086f98a526b388eacc36326f4
MrVicho13 <vicho0202@gmail.com>
2026-01-05 20:11:08 -0300

    refactor(ymap): delete unused menu's remainings.

--- ymap/operators/operators_entity.py
+++ ymap/operators/operators_entity.py
@@ -451,13 +451,3 @@ class VICHO_OT_export_entity_asset(bpy.types.Operator, YmapMixin):
         for f in {'Legacy', 'Enhanced'}:
             col.prop_enum(self, "version", f)
 
-def draw_obj_ctx_menu(self, context):
-    layout = self.layout
-    layout.separator()
-    layout.menu(VICHO_MT_entity_submenu.bl_idname)
-
-def register():
-    bpy.types.VIEW3D_MT_object_context_menu.append(draw_obj_ctx_menu)
-
-def unregister():
-    bpy.types.VIEW3D_MT_object_context_menu.remove(draw_obj_ctx_menu)
