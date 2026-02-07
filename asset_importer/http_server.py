ab0005efc1aa9810206f976a4edb0d0edda9ebdc
MrVicho13 <vicho0202@gmail.com>
2026-02-07 04:24:07 -0300

    feat(asset): import asset category.

--- asset_importer/http_server.py
+++ asset_importer/http_server.py
@@ -1,15 +1,23 @@
 from http.server import BaseHTTPRequestHandler
+from urllib.parse import unquote
 
 imported_asset = ""
+asset_category = ""
 
 class AssetHandler(BaseHTTPRequestHandler):
-    
     def do_POST(self):
-        global imported_asset
-        length = int(self.headers.get('Content-Length', 0))
-        body = self.rfile.read(length).decode()
-        imported_asset = body
-        print("POST recibido:", body)
+        global imported_asset, asset_category
+
+        length = int(self.headers.get("Content-Length", 0))
+        raw = self.rfile.read(length).decode("utf-8", errors="replace")
+
+        left, sep, right = raw.partition(";")
+        imported_asset = unquote(left) if left else ""
+        asset_category = unquote(right) if sep else ""
+
+        print("POST received:", imported_asset)
+        print("Category(s):", asset_category)
+
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.send_header("Access-Control-Allow-Origin", "*")
